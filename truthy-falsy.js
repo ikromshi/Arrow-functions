62d3c854dc308fbc79f4f6aa496b373c9ffcfdf9
ikromshi <numanovikrom454@gmail.com>
2022-01-18 21:49:22 -0500

    working with truthy/falsy values

---
+++ truthy-falsy.js
@@ -0,0 +1,12 @@
+const products = []
+const product = products[0]
+
+// Truthy Values that resollve to true in boolean context
+// Falsy Values that resolve to false in boolean context
+// Falsy => false, 0, empty string, null, undefined
+
+if (product) {
+    console.log("Present")
+} else {
+    console.log("Not Present")
+}
